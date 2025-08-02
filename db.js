00ebb9fb4356997152255e0007464e2deb23914b
unknown <Admin@PX10541HOW10.grupoib.local>
2025-08-01 22:33:25 -0500

    fix: usar submissionDate en lugar de createdAt

--- db.js
+++ db.js
@@ -14,11 +14,14 @@ module.exports = pool;
 */
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
