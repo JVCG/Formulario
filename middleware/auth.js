d69821703c0298fb2198f55869e09c005b69298a
unknown <Admin@PX10541HOW10.grupoib.local>
2025-08-05 13:13:50 -0500

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,18 @@
+/*function requireAuth(req, res, next) {
+  if (req.session && req.session.authenticated) {
+    return next();
+  } else {
+    return res.status(401).json({ message: 'No autorizado' });
+  }
+}*/
 function requireAuth(req, res, next) {
+  console.log('Sesión actual:', req.session); // 👈 Agregado
   if (req.session && req.session.authenticated) {
     return next();
   } else {
     return res.status(401).json({ message: 'No autorizado' });
   }
 }
+
 
 module.exports = requireAuth;
